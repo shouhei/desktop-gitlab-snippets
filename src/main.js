a9f021fdc285261fe371fe79efe4c7c7bf313287
山口将平 <shouhei.yamaguchi.be@gmail.com>
2017-04-23 14:20:27 +0900

    Add vue material

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
 import App from './App.vue'
 
+Vue.use(VueMaterial)
 new Vue({
   el: '#app',
   render: h => h(App)
