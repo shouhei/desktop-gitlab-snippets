3296612f0773f7d95d96d532c8ad18758cdf975c
山口将平 <shouhei.yamaguchi.be@gmail.com>
2017-04-23 20:08:40 +0900

    Add side nav for setting

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
+  <div>
     <md-toolbar>
-    <h1 class="md-title">{{project_name}}</h1>
+      <h1 class="md-title" style="flex: 1">{{project_name}}</h1>
+      <md-button class="md-icon-button" @click.native="toggleRightSidenav">
+          <md-icon>settings</md-icon>
+      </md-button>
     </md-toolbar>
+    <md-sidenav class="md-right" ref="rightSidenav" @open="open('Right')" @close="close('Right')">
+      <md-toolbar>
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Sidenav content</h3>
+        </div>
+      </md-toolbar>
+      <form novalidate @submit.stop.prevent="submit">
+        <md-input-container>
+          <label>Domain</label>
+          <md-input v-model="domain"></md-input>
+        </md-input-container>
+        <md-input-container  md-has-password>
+          <label>Private Token</label>
+          <md-input type="password" v-model="private_token"></md-input>
+        </md-input-container>
+      </form>
+    </md-sidenav>
+  </div>
 </template>
 
 <script>
@@ -9,7 +31,23 @@ export default {
   name: 'app',
   data () {
     return {
-      project_name: 'Desktop GitLab Snippets'
+      project_name: 'Desktop GitLab Snippets',
+      domain: 'gitlab.com',
+      private_token: 'private_token'
+    }
+  },
+  methods: {
+    toggleRightSidenav() {
+      this.$refs.rightSidenav.toggle();
+    },
+    closeRightSidenav() {
+      this.$refs.rightSidenav.close();
+    },
+    open(ref) {
+      console.log('Opened: ' + ref);
+    },
+    close(ref) {
+      console.log('Closed: ' + ref);
     }
   }
 }
